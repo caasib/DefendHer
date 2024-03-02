5b6df889f9315c890181359f1f7937aacea0046e
caasib <cboakye@purdue.edu>
2024-03-02 14:54:24 -0500

    changed redirect of login

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def login():
         username = request.form['username']
         password = request.form['password']  
         if username in users.keys() and password == users[username]:
-            return render_template('Courses.html')
+            return render_template('front.html')
         else:
             error = "Invalid credentials. Please try again."
     return render_template('login.html', error=error)
@@ -23,6 +23,10 @@ def login():
 @app.route('/Courses', methods=['GET', 'POST'])
 def course_page():
     return render_template('Courses.html')
+
+@app.route('/front', method=['GET', 'POST'])
+def front_page():
+    return render_template('front.html')
 
 
 if __name__ == "__main__":
