6f183d2b88a8f924692bafe027044dc56562ecca
caasib <cboakye@purdue.edu>
2024-03-02 14:11:38 -0500

    wefefwd

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-users = {'exampleuser':'examplepass', 'admin':'innovateher'}
+users = {'example':'example', 'admin':'innovateher', 'your':'mom'}
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
-    return render_template('login.html')
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -15,10 +15,14 @@ def login():
         username = request.form['username']
         password = request.form['password']  
         if username in users.keys() and password == users[username]:
-            error = "Yay!"
+            return render_template('Courses.html')
         else:
             error = "Invalid credentials. Please try again."
     return render_template('login.html', error=error)
+
+@app.route('/Courses', methods=['GET', 'POST'])
+def course_page():
+    return render_template('Courses.html')
 
 
 if __name__ == "__main__":
