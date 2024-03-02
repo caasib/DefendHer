78e1a985dcdee4b07471cdc7ac77db4888311f58
caasib <cboakye@purdue.edu>
2024-03-02 14:49:32 -0500

    fixed medha's dumb code

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 users = {'example':'example', 'admin':'innovateher', 'your':'mom'}
 
