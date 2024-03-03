21efc1ecd6d32991475bca2d80cfc0150fa9fd04
medha <shindem@purdue.edu>
2024-03-03 03:28:07 -0500

    SDFFA4ZVDSi W   F82e;

--- static/assets/js/defensequiz.js
+++ static/assets/js/defensequiz.js
@@ -86,113 +86,52 @@ var prompts = [
         weight: -5
     }
     ]
-    
-    // For each prompt, create a list item to be inserted in the list group
-    function createPromptItems() {
-    
-        for (var i = 0; i < prompts.length; i++) {
-            var prompt_li = document.createElement('li');
-            var prompt_p = document.createElement('p');
-            var prompt_text = document.createTextNode(prompts[i].prompt);
-    
-            prompt_li.setAttribute('class', 'list-group-item prompt');
-            prompt_p.appendChild(prompt_text);
-            prompt_li.appendChild(prompt_p);
-    
-            document.getElementById('quiz').appendChild(prompt_li);
-        }
+
+// Function to create prompt items
+function createPromptItems() {
+    for (var i = 0; i < prompts.length; i++) {
+        var prompt_li = document.createElement('li');
+        var prompt_p = document.createElement('p');
+        var prompt_text = document.createTextNode(prompts[i].prompt);
+
+        prompt_li.setAttribute('class', 'list-group-item prompt');
+        prompt_p.appendChild(prompt_text);
+        prompt_li.appendChild(prompt_p);
+
+        document.getElementById('quiz').appendChild(prompt_li);
     }
-    
-    // For each possible value, create a button for each to be inserted into each li of the quiz
-    // function createValueButtons() {
-        
-    // 	for (var li_index = 0; li_index < prompts.length; li_index++) {
-    // 		for (var i = 0; i < prompt_values.length; i++) {
-    // 			var val_button = document.createElement('button');
-    // 			var val_text = document.createTextNode(prompt_values[i].value);
-    
-    // 			val_button.setAttribute('class', 'value-btn btn ' + prompt_values[i].class);
-    // 			val_button.appendChild(val_text);
-    
-    // 			document.getElementsByClassName('prompt')[li_index].appendChild(val_button);
-    // 		}
-    // 	}
-    // }
-    function createValueButtons() {
-        for (var li_index = 0; li_index < prompts.length; li_index++) {
-            var group = document.createElement('div');
-            group.className = 'btn-group btn-group-justified';
-    
-            for (var i = 0; i < prompt_values.length; i++) {
-                var btn_group = document.createElement('div');
-                btn_group.className = 'btn-group';
-    
-                var button = document.createElement('button');
-                var button_text = document.createTextNode(prompt_values[i].value);
-                button.className = 'group' + li_index + ' value-btn btn ' + prompt_values[i].class;
-                button.appendChild(button_text);
-    
-                btn_group.appendChild(button);
-                group.appendChild(btn_group);
-            }
-    
-            document.getElementsByClassName('prompt')[li_index].appendChild(group);
+}
+
+// Function to create value buttons
+function createValueButtons() {
+    for (var li_index = 0; li_index < prompts.length; li_index++) {
+        var group = document.createElement('div');
+        group.className = 'btn-group btn-group-justified';
+
+        for (var i = 0; i < prompt_values.length; i++) {
+            var btn_group = document.createElement('div');
+            btn_group.className = 'btn-group';
+
+            var button = document.createElement('button');
+            var button_text = document.createTextNode(prompt_values[i].value);
+            button.className = 'group' + li_index + ' value-btn btn ' + prompt_values[i].class;
+            button.appendChild(button_text);
+
+            btn_group.appendChild(button);
+            group.appendChild(btn_group);
         }
-    }    
-    
-    createPromptItems();
-    createValueButtons();
-    
-    // Keep a running total of the values they have selected. If the total is negative, the user is introverted. If positive, user is extroverted.
-    // Calculation will sum all of the answers to the prompts using weight of the value * the weight of the prompt.
-    var total = 0;
-    
-    // Get the weight associated to group number
-    function findPromptWeight(prompts, group) {
-        var weight = 0;
-    
-        for (var i = 0; i < prompts.length; i++) {
-            if (prompts[i].class === group) {
-                weight = prompts[i].weight;
-            }
-        }
-    
-        return weight;
+
+        document.getElementsByClassName('prompt')[li_index].appendChild(group);
     }
-    
-    // Get the weight associated to the value
-    function findValueWeight(values, value) {
-        var weight = 0;
-    
-        for (var i = 0; i < values.length; i++) {
-            if (values[i].value === value) {
-                weight = values[i].weight;
-            }
-        }
-    
-        return weight;
-    }
-    
-// When user clicks a value to agree/disagree with the prompt
-$(document).on('click', '.value-btn', function () {
-    var classList = $(this).attr('class');
-    var classArr = classList.split(" ");
-    var this_group = classArr[0];
-    var selectedValue = $(this).text();
+}
 
-    // Remove active class from other buttons in the same group
-    $('.' + this_group).removeClass('active');
-    
-    // Reset background color of all buttons in the same group
-    $('.' + this_group).css('background-color', '');
+createPromptItems();
+createValueButtons();
 
-    // Add active class to the clicked button
-    $(this).addClass('active');
+var total = 0;
 
-    // Darken the background color of the clicked button
-    $(this).css('background-color', '#333');
-
-    // Calculate the total score based on user selections
+// Function to calculate total score
+function calculateTotal() {
     total = 0;
     $('.value-btn.active').each(function () {
         var buttonClassList = $(this).attr('class');
@@ -203,38 +142,65 @@ $(document).on('click', '.value-btn', function () {
     });
 
     console.log(total);
+}
+
+// Function to find prompt weight
+function findPromptWeight(prompts, group) {
+    var weight = 0;
+    for (var i = 0; i < prompts.length; i++) {
+        if (prompts[i].class === group) {
+            weight = prompts[i].weight;
+        }
+    }
+    return weight;
+}
+
+// Function to find value weight
+function findValueWeight(values, value) {
+    var weight = 0;
+    for (var i = 0; i < values.length; i++) {
+        if (values[i].value === value) {
+            weight = values[i].weight;
+        }
+    }
+    return weight;
+}
+
+// Event listener for value buttons
+$(document).on('click', '.value-btn', function () {
+    var classList = $(this).attr('class');
+    var classArr = classList.split(" ");
+    var this_group = classArr[0];
+    var selectedValue = $(this).text();
+
+    $('.' + this_group).removeClass('active');
+    $('.' + this_group).css('background-color', '');
+    $(this).addClass('active');
+    $(this).css('background-color', '#333');
+
+    calculateTotal();
 });
 
-// When user clicks the submit button
+// Event listener for submit button
 $(document).on('click', '#submit-btn', function () {
     $('.results').removeClass('hide').addClass('show');
 
     if (total < -15) {
-        document.getElementById('results').innerHTML = '<b>Beginner</b><br><br>\
-            Based on your quiz responses, you have been placed in the Beginner level. This means that you are just starting your journey in self-defense and may have limited experience or training in this area. In the Beginner course, you will learn foundational self-defense techniques, such as basic strikes, blocks, and escapes, designed to help you build confidence and develop essential skills for personal safety. Through structured lessons and practice sessions, you will gradually improve your ability to protect yourself in various situations.\
-            ';
-        /// Create and show Course 1 button
         var course1Button = document.createElement('button');
         course1Button.setAttribute('id', 'course1-btn');
         course1Button.setAttribute('class', 'button');
         course1Button.textContent = 'Take Course 1';
         document.getElementById('results').appendChild(course1Button);
 
-        // Add event listener to Course 1 button
         course1Button.addEventListener('click', function() {
-            window.location.href = './course1'; // Redirect to course1.html
+            window.location.href = './course1.html';
         });
     } else if (total > 15) {
-        document.getElementById('results').innerHTML = '<b>Advanced</b><br><br>\
-            Based on your quiz responses, you have been placed in the Advanced level. This signifies that you possess a high level of skill and proficiency in self-defense. In the Advanced course, you will refine and master advanced techniques, strategies, and tactics for effectively defending yourself in challenging situations. You will learn advanced striking combinations, intricate grappling maneuvers, and specialized defense techniques tailored to various threats. Additionally, you will focus on developing mental toughness, situational awareness, and the ability to adapt to dynamic encounters. Prepare to elevate your self-defense skills to the highest level and become a formidable defender.\
-            ';
+        document.getElementById('results').innerHTML = '<b>Advanced</b><br><br>Based on your quiz responses, you have been placed in the Advanced level.';
     } else {
-        document.getElementById('results').innerHTML = '<b>Intermediate</b><br><br>\
-            Based on your quiz responses, you have been placed in the Intermediate level. This indicates that you have some experience or training in self-defense and are ready to further develop your skills. In the Intermediate course, you will build upon the foundational techniques learned in the Beginner level and delve deeper into more advanced concepts. You will learn techniques for handling more complex situations, refining your striking and grappling skills, and enhancing your overall self-defense proficiency. With dedication and practice, you will continue to progress towards becoming a more confident and capable defender.\
-            '
+        document.getElementById('results').innerHTML = '<b>Intermediate</b><br><br>Based on your quiz responses, you have been placed in the Intermediate level.';
     }
 
-    // Hide the quiz after they submit their results
     $('#quiz').addClass('hide');
     $('#submit-btn').addClass('hide');
 });
